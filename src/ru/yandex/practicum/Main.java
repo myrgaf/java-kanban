53bb72e016b811c11d0aa80f7a6c4c03e6875fd0
Alex Petrov <alexpetrov997@gmail.com>
2025-11-02 11:48:41 +0300

    Добавить тесты на функционал сохранения, поменять доступ на protected полей InMemoryTaskManager для обращения в классах-наследниках

--- src/ru/yandex/practicum/Main.java
+++ src/ru/yandex/practicum/Main.java
@@ -1,8 +1,12 @@
 package ru.yandex.practicum;
 
+import ru.yandex.practicum.manager.FileBackedTaskManager;
 import ru.yandex.practicum.manager.Managers;
 import ru.yandex.practicum.manager.TaskManager;
 import ru.yandex.practicum.models.*;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class Main {
@@ -31,7 +35,7 @@ public class Main {
         System.out.println(" — ".repeat(40));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TaskManager manager = Managers.getDefault();
 
         // Обычные задачи
@@ -56,5 +60,26 @@ public class Main {
         manager.getEpic(epic2.getId());
 
         printAllTasks(manager);
+
+
+        // Проверка функционала сохранения в файл.csv и восстановления из файла.csv
+
+        File tempFile = File.createTempFile("test", ".csv");
+        System.out.println("Файл: " + tempFile.getAbsolutePath());
+
+        FileBackedTaskManager backendManager = new FileBackedTaskManager(tempFile);
+
+        Task task = backendManager.createTask(new Task("Тест", "Описание", Status.NEW));
+        Epic epic = backendManager.createEpic(new Epic("Эпик", "Описание", Status.NEW));
+        Subtask sub = backendManager.createSubtask(new Subtask("Подзадача", "Описание", Status.DONE, epic.getId()));
+
+        System.out.println("Сохранено. Перезагружаем...");
+
+        FileBackedTaskManager loaded = FileBackedTaskManager.loadFromFile(tempFile);
+
+        System.out.println("Задачи: " + loaded.getAllTasks().size());
+        System.out.println("Эпики: " + loaded.getAllEpics().size());
+        System.out.println("Подзадачи: " + loaded.getAllSubtasks().size());
+        System.out.println("Статус эпика: " + loaded.getEpic(epic.getId()).getStatus());
     }
 }
